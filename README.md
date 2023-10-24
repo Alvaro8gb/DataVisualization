e53fa4e76a145ea69dc75a9418aca9a96595c032
Álvaro García Barragán <79256834+Alvaro8gb@users.noreply.github.com>
2023-10-24 20:50:42 +0200

    set up repo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# DataVisualization
-A Shinny App, a descriptive analisis of Lung Cancer
+
+# Data Visualization Practical Work Repository
+
+Welcome to the Data Visualization Practical Work repository for the MSc Data Science program at UPM. This repository hosts assignments and projects related to the course. You can find the detailed description of the practical work in the [Visual Analytics PDF](./Vis-00c-VisualAnalytics.pdf).
+
+## Projects in this Repository
+
+1. **Shiny Tutorial**: This project is designed to help you learn the Shiny framework for developing data visualization applications. It serves as a hands-on tutorial, providing you with valuable insights into creating interactive data visualizations. [LINK](./ShinnyTutorial/)
+
+2. **Shiny App - Lung Cancer Analysis**: In this project, you will find a Shiny application that offers a descriptive analysis of lung cancer data. The application provides an interactive platform for exploring and visualizing data related to lung cancer, making it an informative tool for research and analysis. [LINK](./lungCancerApp/)
+
+
+
---
+++ ShinnyTutorial/basic_interface.R
@@ -0,0 +1,33 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("My Shiny App"),
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel(
+      p("p creates a paragraph of text."),
+      p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
+      strong("strong() makes bold text."),
+      em("em() creates italicized (i.e, emphasized) text."),
+      br(),
+      code("code displays your text similar to computer code"),
+      div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+      br(),
+      p("span does the same thing as div, but it works with",
+        span("groups of words", style = "color:blue"),
+        "that appear inside a paragraph."),
+
+      img(src = "example.jpg", height = 200, width = 200)
+
+    )
+  )
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
+
