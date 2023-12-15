7c0e76d5c71879464aa80ad8927bd9239d31fd09
Maxxsss <maxime@kermagoret.net>
2023-12-15 16:37:47 +0100

    idiom 1 global idiom

--- App/app/server.R
+++ App/app/server.R
@@ -1,26 +1,60 @@
 # server.R
 library(shiny)
-library(quantmod)
+library(ggplot2)
+
+dataset <- read.csv("../dataset/ds-final.csv")
 
 members <- list("Alvaro", "Maxi", "Mikel")
 
 server <- function(input, output) {
-
   output$members <- renderText({
     paste("Authors:", paste(members, collapse = ", "))
   })
-
+  
   output$selectedValues <- renderText({
     paste("Selected values:",
-          "\nAge:", input$age,
-          "\nGender:", input$gender,
-          "\nBMI:", input$bmi,
-          "\nSymptoms:", ifelse(length(input$symptoms) > 0, paste(input$symptoms, collapse = ", "), "None"),
-          "\nWBC:", input$wbc,
-          "\nRBC:", input$rbc,
-          "\nHGB:", input$hgb,
-          "\nPlatelet:", input$plat
-    )
+          "\nAge:", input$age[1], "-", input$age[2],
+          ",\nGender:", input$gender,
+          ",\nBMI:", input$bmi[1], "-", input$bmi[2],
+          ",\nSymptoms:", ifelse(length(input$symptoms) > 0, paste(input$symptoms, collapse = ", "), "None"),
+          ",\nWBC:", input$wbc[1], "-", input$wbc[2],
+          ",\nRBC:", input$rbc[1], "-", input$rbc[2],
+          ",\nHGB:", input$hgb[1], "-", input$hgb[2],
+          ",\nPlatelet:", input$plat[1], "-", input$plat[2])
   })
-
+  
+  output$idiom1scatterplot <- renderPlot({
+    req(input$xaxis,input$yaxis)
+    x_axis <- input$xaxis
+    y_axis <- input$yaxis
+    
+    # Check if "Age" is selected for X or Y axis
+    if (x_axis == "Age") {
+      x_limits <- c(as.numeric(input$age[1]), as.numeric(input$age[2]))
+    } else if (x_axis == sym("BMI")) {
+      x_limits <- c(as.numeric(input$bmi[1]), as.numeric(input$bmi[2]))
+    } else if (x_axis == "Baselinehistological.staging"){ ##  define x_limits for other variables
+      x_limits <- c(1, 4)
+    }else if (x_axis == "Baseline.histological.Grading"){ ##  define x_limits for other variables
+      x_limits <- c(3, 16)
+    }
+    
+    if (y_axis == "Age") {
+      y_limits <- c(as.numeric(input$age[1]), as.numeric(input$age[2]))
+    }else if (y_axis == "BMI") {
+      y_limits <- c(as.numeric(input$bmi[1]), as.numeric(input$bmi[2]))
+    }else if (y_axis == "Baselinehistological.staging"){ ##  define y_limits for other variables
+      y_limits <- c(1, 4)
+    }else if (y_axis == "Baseline.histological.Grading"){ ##  define y_limits for other variables
+      y_limits <- c(3, 16)
+    }
+    
+    p <- ggplot(data = dataset, aes(x = as.numeric(.data[[input$xaxis]]), y = as.numeric(.data[[input$yaxis]]))) +
+      geom_point() +
+      theme(legend.position = "top") +
+      xlim(x_limits) +
+      ylim(y_limits) +
+      labs(title = paste("Scatterplot", input$xaxis, " vs ", input$yaxis), x = input$xaxis, y = input$yaxis)
+    return(p)
+  })
 }
--- App/app/ui.R
+++ App/app/ui.R
@@ -1,72 +1,39 @@
 library(shiny)
 
+dataset <- read.csv("../dataset/ds-final.csv")
+
 departament <- list("d1", "d2")
 
 ui <- fluidPage(
   titlePanel("Hepatitis Visualization App"),
   verbatimTextOutput("members"),
   sidebarPanel(
-      sliderInput("age", "Age", min = 32, max = 61, value = c(32, 61)),
-      selectInput("gender", "Gender", choices = c("Male", "Female")),
-      sliderInput("bmi", "BMI (Body Mass Index)", min = 22, max = 35, value = c(22, 35)),
-      checkboxGroupInput("symptoms", "Symptoms", choices = c("Fever", "Nausea/Vomiting", "Headache", "Diarrhea", "Fatigue and generalized", "Bone ache", "Jaundice", "Epigastric pain")),
-      sliderInput("wbc", "WBC (White blood cell)", min = 299, max = 12101, value = c(299, 12101)),
-      sliderInput("rbc", "RBC (Red blood cells)", min = 3816422, max = 5018451, value = c(3816422, 5018451)),
-      sliderInput("hgb", "HGB (Hemoglobin)", min = 10, max = 15, value = c(10, 15)),
-      sliderInput("plat", "Platelet", min = 93013, max = 226464, value = c(93013, 226464))
-    ),
-    
-    mainPanel(
-      textOutput("selectedValues")
-    ),
-  fluidRow(
-    style = "background-color: #F4F5F5;padding: 5px;",
-    helpText("1. Idiom."),
-    column(5, selectInput("var1",
-      label = "Choose a personal information",
-      choices = list(
-        "Department",
-        "HispanicLatino",
-        "CitizenDesc",
-        "MaritalDesc",
-        "RaceDesc",
-        "RecruitmentSource",
-        "Sex"
-      ),
-      selected = "Department"
-    )),
-    column(5, selectInput("var2",
-      label = "Choose a working situation indicator",
-      choices = list(
-        "Salary",
-        "PerformanceScore",
-        "EmpSatisfaction"
-      ),
-      selected = "Salary"
-    )),
+    selectInput("xaxis", "X-axis", choices = c("BMI", "Baselinehistological.staging", "Baseline.histological.Grading", "Age")),
+    selectInput("yaxis", "Y-axis", choices = c("Baselinehistological.staging", "Baseline.histological.Grading", "BMI", "Age")),
+    sliderInput("age", "Age", min = min(dataset$Age), max = max(dataset$Age), value = c(min(dataset$Age), max(dataset$Age))),
+    selectInput("gender", "Gender", choices = c("Male", "Female")),
+    sliderInput("bmi", "BMI (Body Mass Index)", min = min(dataset$BMI), max = max(dataset$BMI), value = c(min(dataset$BMI), max(dataset$BMI))),
+    checkboxGroupInput("symptoms", "Symptoms", choices = c("Fever", "Nausea/Vomiting", "Headache", "Diarrhea", "Fatigue and generalized", "Bone ache", "Jaundice", "Epigastric pain")),
+    sliderInput("wbc", "WBC (White blood cell)", min = 299, max = 12101, value = c(299, 12101)),
+    sliderInput("rbc", "RBC (Red blood cells)", min = 3816422, max = 5018451, value = c(3816422, 5018451)),
+    sliderInput("hgb", "HGB (Hemoglobin)", min = 10, max = 15, value = c(10, 15)),
+    sliderInput("plat", "Platelet", min = 93013, max = 226464, value = c(93013, 226464))
   ),
-  fluidRow(
-    style = "background-color: #F4F5F5;padding: 5px;",
-    helpText("2. Idiom"),
-    column(5, selectInput("var",
-      label = "Choose a job industry",
-      choices = c("Production", "IT_IS", "Software_Engineering", "Sales", "Admin_Offices", "Executive_Office"),
-      selected = "Production"
-    )),
-    column(5, sliderInput("range",
-      label = "Range of Salary:",
-      min = 45046, max = 250000, value = c(45046, 250000), pre = "$"
-    )),
-  ),
-  fluidRow(
-    style = "background-color: #F4F5F5;padding: 5px;",
-    helpText("3. Idiom"),
-    column(
-      3,
-      selectInput("depart",
-        label = "Choose the area of the job title",
-        choices = departament, selected = "IT_IS"
-      )
+  
+  mainPanel(
+    textOutput("selectedValues"),
+    fluidRow(
+      style = "background-color: #F4F5F5;padding: 5px;",
+      helpText("1. Idiom."),
+      plotOutput("idiom1scatterplot"),
+    ),
+    fluidRow(
+      style = "background-color: #F4F5F5;padding: 5px;",
+      helpText("2. Idiom"),
+    ),
+    fluidRow(
+      style = "background-color: #F4F5F5;padding: 5px;",
+      helpText("3. Idiom"),
     )
   )
 )
